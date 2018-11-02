b4f86a6181c6e0c01cf60e63493ec6f2a18d285e
andrew <andrew.poltoranos@greenwoodcollege.com>
2018-11-02 12:51:49 -0400

    Added second part

--- Part B.py
+++ Part B.py
@@ -9,6 +9,26 @@
 #grade(49)
 #returns ‘F’
 
+def grade_percent(grade):
+    if int(grade) <= 49:
+        print("You got an F")
+        return "F"
+    elif 50 >= int(grade) >= 64:
+        print("You got a C")
+        return "C"
+    elif 65 >= int(grade) >= 79:
+        print("You got a B")
+        return "B"
+    elif 80 >= int(grade) >= 94:
+        print("You got an A")
+        return "A"
+    elif int(grade) >= 95:
+        print("You got an A+!!!!")
+        return "A+"
+
+
+grade_percent(grade=int(input("What grade did you get? ")))
+
 def doubleEven(n):
     if int(n) % 2 != 0:
         print(-1)
@@ -20,5 +40,6 @@ def doubleEven(n):
     else:
         print(-1)
         return -1
+
 
 doubleEven(n=input("What number would you like? "))
